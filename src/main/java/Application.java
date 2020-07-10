2382b7c3725f6067ad0b80073a68502e052f2098
AndriiShykoriak <andrii.shykoriak@gmail.com>
2020-07-10 23:24:04 +0300

    main class

---
+++ src/main/java/Application.java
@@ -0,0 +1,11 @@
+import map.FamilyMap;
+import map.UniqueNumbers;
+
+public class Application {
+    public static void main(String[] args) {
+        var numbers = new UniqueNumbers();
+        numbers.uniqueNumbers();
+        var familyMap = new FamilyMap();
+        familyMap.fillingMapFamily();
+    }
+}
